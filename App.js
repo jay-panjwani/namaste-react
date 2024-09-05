d8addc85f39dd0d808e3bcff640bc29325b0d060
Jay Panjwani <jaypanjwani39@gmail.com>
2024-09-06 00:51:14 +0530

    episode-1

---
+++ App.js
@@ -0,0 +1,4 @@
+const title = React.createElement('h2', {id: 'jp', class: 'text-danger'}, 'Namaste Duniya from React')
+
+        const rootElement = ReactDOM.createRoot(document.getElementById('container'));
+        rootElement.render(title);
